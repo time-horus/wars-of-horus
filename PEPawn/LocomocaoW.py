fdbc66f33377aaa88b711d464d06c3297eb8d8d0
KingSoapVII <126209081+KingSoapVII@users.noreply.github.com>
2023-10-16 20:14:17 -0300

    Adição dos arquivos que eu utilizei até o momento
    arquivos com os códigos para o jogo funcionar

---
+++ PEPawn/LocomocaoW.py
@@ -0,0 +1,29 @@
+import pygame
+import Sorteio
+import random
+
+from Sorteio import SorteiaW
+
+from pygame.locals import *
+
+
+def locomocaoW(px,py,state,mx,my,largura,altura,event,tela,turno,numero):
+    
+    if round((mx/(largura/8))-0.5) == px and round((my/(largura/8))-0.5) == py and event.type == MOUSEBUTTONDOWN: #faz a comparação da posição no click do mouse com a posição da peça, se for igual:
+        pygame.draw.circle(tela, (125,148,93),((px+0.5)*(largura/8),(py+0.5)*(altura/8)),32) #troca a cor da peça para mostrar seleção
+        pygame.draw.circle(tela, (199, 198, 149),((px+0.5)*(largura/8),(py+0.5)*(altura/8)),30)
+        state = 1
+    if state == 1:
+        pygame.draw.circle(tela, (125,148,93),((px+0.5)*(largura/8),(py+0.5)*(altura/8)),32) #troca a cor da peça para mostrar seleção
+        pygame.draw.circle(tela, (199, 198, 149),((px+0.5)*(largura/8),(py+0.5)*(altura/8)),30)
+        loc = SorteiaW(mx,largura,my,px,py,numero)                                                                                #muda o estado da peça para 1(selecionado)
+
+    if event.type == MOUSEBUTTONUP and state == 1 and loc:
+        pygame.draw.circle(tela, (125,148,93),((px+0.5)*(largura/8),(py+0.5)*(altura/8)),32) #troca a cor da peça para mostrar seleção
+        pygame.draw.circle(tela, (199, 198, 149),((px+0.5)*(largura/8),(py+0.5)*(altura/8)),30)                                           #se click do mouse e State == 1
+        state = 0                                                                                #muda State para 0
+        mx, my = pygame.mouse.get_pos()                                                      #pega a posição do mouse no momento do click
+        px = round((mx/(largura/8))-0.5)                                                    #tratamento da posição do mouse e atribuição na posição da peça
+        py = round((my/(altura/8))-0.5)
+        turno = turno+1
+    return (px,py,state,turno)
---
+++ PEPawn/Sorteio.py
@@ -0,0 +1,55 @@
+import pygame
+import random
+
+import random as rnd
+
+from pygame.locals import *
+
+
+
+
+def SorteiaW(mx,largura,my,px,py,numero):
+    match numero:
+        case 0:
+            loc = (round((mx/(largura/8))-0.5) == px-1 or round((mx/(largura/8))-0.5) == px-1) and round((my/(largura/8))-0.5) == py-2
+            #print("1")
+        case 1:
+            loc = round((mx/(largura/8))-0.5) == px and round((my/(largura/8))-0.5) == py-1
+            #print("2")
+        case 2:
+            loc = round((mx/(largura/8))-0.5) == px and round((my/(largura/8))-0.5) == py-2
+            #print("3")
+        case 3:
+            loc = round((mx/(largura/8))-0.5) == px and round((my/(largura/8))-0.5) == py-3
+            #print("4")
+        case 4:
+            loc = round((mx/(largura/8))-0.5) == px and round((my/(largura/8))-0.5) == py-4
+            #print("5")
+        case 5:
+            loc = round((mx/(largura/8))-0.5) == px and round((my/(largura/8))-0.5) == py-5
+        
+    return(loc)
+
+
+def SorteiaB(mx,largura,my,px,py,numero):
+    
+    match numero:
+        case 0:
+            loc = round((mx/(largura/8))-0.5) == px and round((my/(largura/8))-0.5) == py
+
+        case 1:
+            loc = round((mx/(largura/8))-0.5) == px and round((my/(largura/8))-0.5) == py+1
+
+        case 2:
+            loc = round((mx/(largura/8))-0.5) == px and round((my/(largura/8))-0.5) == py+2
+
+        case 3:
+            loc = round((mx/(largura/8))-0.5) == px and round((my/(largura/8))-0.5) == py+3
+
+        case 4:
+            loc = round((mx/(largura/8))-0.5) == px and round((my/(largura/8))-0.5) == py+4
+
+        case 5:
+            loc = round((mx/(largura/8))-0.5) == px and round((my/(largura/8))-0.5) == py+5
+    
+    return(loc)
