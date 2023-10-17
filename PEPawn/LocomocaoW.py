a1e359b730f4713fbecceaf1975873fcf9fc7622
KingSoapVII <126209081+KingSoapVII@users.noreply.github.com>
2023-10-17 12:17:41 -0300

    Update LocomocaoW.py
    Alterações feitas para que os podered funcionem

--- PEPawn/LocomocaoW.py
+++ PEPawn/LocomocaoW.py
@@ -1,13 +1,14 @@
 import pygame
 import Sorteio
-import random
+import Poderes
 
+from Poderes import PoderW
 from Sorteio import SorteiaW
 
 from pygame.locals import *
 
 
-def locomocaoW(px,py,state,mx,my,largura,altura,event,tela,turno,numero):
+def locomocaoW(px,py,state,mx,my,largura,altura,event,tela,turno,numero,poder,cont):
     
     if round((mx/(largura/8))-0.5) == px and round((my/(largura/8))-0.5) == py and event.type == MOUSEBUTTONDOWN: #faz a comparação da posição no click do mouse com a posição da peça, se for igual:
         pygame.draw.circle(tela, (125,148,93),((px+0.5)*(largura/8),(py+0.5)*(altura/8)),32) #troca a cor da peça para mostrar seleção
@@ -16,7 +17,12 @@ def locomocaoW(px,py,state,mx,my,largura,altura,event,tela,turno,numero):
     if state == 1:
         pygame.draw.circle(tela, (125,148,93),((px+0.5)*(largura/8),(py+0.5)*(altura/8)),32) #troca a cor da peça para mostrar seleção
         pygame.draw.circle(tela, (199, 198, 149),((px+0.5)*(largura/8),(py+0.5)*(altura/8)),30)
-        loc = SorteiaW(mx,largura,my,px,py,numero)                                                                                #muda o estado da peça para 1(selecionado)
+        if cont%3 == 0:
+            loc = PoderW(mx,largura,my,px,py,poder)
+        else:
+            loc = SorteiaW(mx,largura,my,px,py,numero)
+
+                                                                                          #muda o estado da peça para 1(selecionado)
 
     if event.type == MOUSEBUTTONUP and state == 1 and loc:
         pygame.draw.circle(tela, (125,148,93),((px+0.5)*(largura/8),(py+0.5)*(altura/8)),32) #troca a cor da peça para mostrar seleção
