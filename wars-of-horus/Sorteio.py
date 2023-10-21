5406fd51d1a380fe4e46f2bb8a86cff3e0187f2e
KingSoapVII <126209081+KingSoapVII@users.noreply.github.com>
2023-10-21 10:12:20 -0300

    Update Sorteio.py
    Vacalo comendo tudo

--- wars-of-horus/Sorteio.py
+++ wars-of-horus/Sorteio.py
@@ -1,47 +1,45 @@
 import pygame
 from pygame.locals import *
 
-
-def SorteiaW(mx,largura,my,px,py,numero):
-    match numero:
+def PoderW(mx,largura,my,px,py,poder):
+    match poder:
         case 0:
-            loc = round((mx/(largura/8))-0.5) == px and round((my/(largura/8))-0.5) == py-1 #Peão
-            print("Múmia")
+            loc = round((mx/(largura/8))-0.5) or round((my/(largura/8))-0.5)
+            print("Plot Twist")
+
         case 1:
-            loc = ( (round((mx/(largura/8))-0.5) == px+1 or round((mx/(largura/8))-0.5) == px-1) and ((round((my/(largura/8))-0.5) == py-2)) or ((((round((mx/largura/8))-0.5) == px-2 or round((mx/largura/8))-0.5) == px+2)  and ((round((my/largura/8))-0.5) == py-1))) #Cavalo
-            print("Camelo")
+            loc = ((round((mx/(largura/8))-0.5) == px+1 or round((mx/(largura/8))-0.5) == px-1) and ((round((my/(largura/8))-0.5) == py-3))) or ((round((mx/(largura/8))-0.5) == px+3 or round((mx/(largura/8))-0.5) == px-3) and ((round((my/(largura/8))-0.5) == py-1))) or ((round((mx/(largura/8))-0.5) == px+1 or round((mx/(largura/8))-0.5) == px-1) and ((round((my/(largura/8))-0.5) == py+3))) or ((round((mx/(largura/8))-0.5) == px+3 or round((mx/(largura/8))-0.5) == px-3) and ((round((my/(largura/8))-0.5) == py+1)))
+            print("Camelo nos Esteroides de Cobre")
+
         case 2:
-            loc = (round((mx/(largura/8))-0.5) == px+1 and round((my/(largura/8))-0.5) == py-1) or (round((mx/(largura/8))-0.5) == px+2 and round((my/(largura/8))-0.5) == py-2) or (round((mx/(largura/8))-0.5) == px+3 and round((my/(largura/8))-0.5) == py-3)or (round((mx/(largura/8))-0.5) == px+4 and round((my/(largura/8))-0.5) == py-4)or (round((mx/(largura/8))-0.5) == px+5 and round((my/(largura/8))-0.5) == py-5)or (round((mx/(largura/8))-0.5) == px+6 and round((my/(largura/8))-0.5) == py-6)or (round((mx/(largura/8))-0.5) == px+7 and round((my/(largura/8))-0.5) == py-7) or (round((mx/(largura/8))-0.5) == px-1 and round((my/(largura/8))-0.5) == py+1) or (round((mx/(largura/8))-0.5) == px-2 and round((my/(largura/8))-0.5) == py+2) or (round((mx/(largura/8))-0.5) == px-3 and round((my/(largura/8))-0.5) == py+3) or (round((mx/(largura/8))-0.5) == px-4 and round((my/(largura/8))-0.5) == py+4) or (round((mx/(largura/8))-0.5) == px-5 and round((my/(largura/8))-0.5) == py+5) or (round((mx/(largura/8))-0.5) == px-6 and round((my/(largura/8))-0.5) == py+6) or (round((mx/(largura/8))-0.5) == px-7 and round((my/(largura/8))-0.5) == py+7) or (round((mx/(largura/8))-0.5) == px-1 and round((my/(largura/8))-0.5) == py-1) or (round((mx/(largura/8))-0.5) == px-2 and round((my/(largura/8))-0.5) == py-2) or (round((mx/(largura/8))-0.5) == px-3 and round((my/(largura/8))-0.5) == py-3)or (round((mx/(largura/8))-0.5) == px-4 and round((my/(largura/8))-0.5) == py-4)or (round((mx/(largura/8))-0.5) == px-5 and round((my/(largura/8))-0.5) == py-5)or (round((mx/(largura/8))-0.5) == px-6 and round((my/(largura/8))-0.5) == py-6)or (round((mx/(largura/8))-0.5) == px-7 and round((my/(largura/8))-0.5) == py-7) or (round((mx/(largura/8))-0.5) == px+1 and round((my/(largura/8))-0.5) == py+1) or (round((mx/(largura/8))-0.5) == px+2 and round((my/(largura/8))-0.5) == py+2) or (round((mx/(largura/8))-0.5) == px+3 and round((my/(largura/8))-0.5) == py+3) or (round((mx/(largura/8))-0.5) == px+4 and round((my/(largura/8))-0.5) == py+4) or (round((mx/(largura/8))-0.5) == px+5 and round((my/(largura/8))-0.5) == py+5) or (round((mx/(largura/8))-0.5) == px+6 and round((my/(largura/8))-0.5) == py+6) or (round((mx/(largura/8))-0.5) == px+7 and round((my/(largura/8))-0.5) == py+7)
-            print("Sacerdote")
+            loc = round((mx/(largura/8))-0.5) or round((my/(largura/8))-0.5)
+            print("Bonk")
         case 3:
-            loc =  (round((mx/(largura/8))-0.5) == px and (8 >= round((my/(largura/8))-0.5) >= 0)) or (round((my/(largura/8))-0.5) == py and (8 >= round((mx/(largura/8))-0.5) >=0)) #Torre
-            print("Pirâmide")
+            loc = round((mx/(largura/8))-0.5) == px and round((my/(largura/8))-0.5) >= py
+            print("Moon Walk")
         case 4:
-            loc = (round((mx/(largura/8))-0.5) == px+1 and round((my/(largura/8))-0.5) == py-1) or (round((mx/(largura/8))-0.5) == px+2 and round((my/(largura/8))-0.5) == py-2) or (round((mx/(largura/8))-0.5) == px+3 and round((my/(largura/8))-0.5) == py-3)or (round((mx/(largura/8))-0.5) == px+4 and round((my/(largura/8))-0.5) == py-4)or (round((mx/(largura/8))-0.5) == px+5 and round((my/(largura/8))-0.5) == py-5)or (round((mx/(largura/8))-0.5) == px+6 and round((my/(largura/8))-0.5) == py-6)or (round((mx/(largura/8))-0.5) == px+7 and round((my/(largura/8))-0.5) == py-7) or (round((mx/(largura/8))-0.5) == px-1 and round((my/(largura/8))-0.5) == py+1) or (round((mx/(largura/8))-0.5) == px-2 and round((my/(largura/8))-0.5) == py+2) or (round((mx/(largura/8))-0.5) == px-3 and round((my/(largura/8))-0.5) == py+3) or (round((mx/(largura/8))-0.5) == px-4 and round((my/(largura/8))-0.5) == py+4) or (round((mx/(largura/8))-0.5) == px-5 and round((my/(largura/8))-0.5) == py+5) or (round((mx/(largura/8))-0.5) == px-6 and round((my/(largura/8))-0.5) == py+6) or (round((mx/(largura/8))-0.5) == px-7 and round((my/(largura/8))-0.5) == py+7) or (round((mx/(largura/8))-0.5) == px-1 and round((my/(largura/8))-0.5) == py-1) or (round((mx/(largura/8))-0.5) == px-2 and round((my/(largura/8))-0.5) == py-2) or (round((mx/(largura/8))-0.5) == px-3 and round((my/(largura/8))-0.5) == py-3)or (round((mx/(largura/8))-0.5) == px-4 and round((my/(largura/8))-0.5) == py-4)or (round((mx/(largura/8))-0.5) == px-5 and round((my/(largura/8))-0.5) == py-5)or (round((mx/(largura/8))-0.5) == px-6 and round((my/(largura/8))-0.5) == py-6)or (round((mx/(largura/8))-0.5) == px-7 and round((my/(largura/8))-0.5) == py-7) or (round((mx/(largura/8))-0.5) == px+1 and round((my/(largura/8))-0.5) == py+1) or (round((mx/(largura/8))-0.5) == px+2 and round((my/(largura/8))-0.5) == py+2) or (round((mx/(largura/8))-0.5) == px+3 and round((my/(largura/8))-0.5) == py+3) or (round((mx/(largura/8))-0.5) == px+4 and round((my/(largura/8))-0.5) == py+4) or (round((mx/(largura/8))-0.5) == px+5 and round((my/(largura/8))-0.5) == py+5) or (round((mx/(largura/8))-0.5) == px+6 and round((my/(largura/8))-0.5) == py+6) or (round((mx/(largura/8))-0.5) == px+7 and round((my/(largura/8))-0.5) == py+7) or (round((mx/(largura/8))-0.5) == px and (8 >= round((my/(largura/8))-0.5) >= 0)) or (round((my/(largura/8))-0.5) == py and (8 >= round((mx/(largura/8))-0.5) >=0))#Rainha
-            print("Isis")
-        case 5:
-            loc = ((round((mx/(largura/8))-0.5) == px-1 or (round((mx/(largura/8))-0.5) == px +1)) and (round((my/(largura/8))-0.5) == py) or (round((mx/(largura/8))-0.5) == px) or ((round((my/(largura/8))-0.5) == py-1 or round((my/(largura/8))-0.5) == py+1) and  (round((mx/(largura/8))-0.5) == px or round((mx/(largura/8))-0.5) == px)) or (round((mx/(largura/8))-0.5) == px-1 or (round((mx/(largura/8))-0.5) == px+1) and ((round((my/(largura/8))-0.5) == py-1 or round((my/(largura/8))-0.5) == py+1))))#rei
-            print("Hórus")
+            loc = (round((mx/(largura/8))-0.5) == px+1 and round((my/(largura/8))-0.5) <= py+1) or (round((mx/(largura/8))-0.5) == px-1 and round((my/(largura/8))-0.5) <= py-1) or (round((mx/(largura/8))-0.5) == px-1 and round((my/(largura/8))-0.5) <= py+1) or (round((mx/(largura/8))-0.5) == px+1 and round((my/(largura/8))-0.5) <= py-1)
+            print("De volta para o futuro")
+        
+        
     return(loc)
 
-def SorteiaB(mx,largura,my,px,py,numero):
-    match numero:
+def PoderB(mx,largura,my,px,py,poder):
+    match poder:
         case 0:
-            loc = round((mx/(largura/8))-0.5) == px and round((my/(largura/8))-0.5) == py+1 #Peão
-            print("Múmia")
+            loc = round((mx/(largura/8))-0.5) or round((my/(largura/8))-0.5)
+            print("Plot Twist")
         case 1:
-            loc = ( (round((mx/(largura/8))-0.5) == px+1 or round((mx/(largura/8))-0.5) == px-1) and ((round((my/(largura/8))-0.5) == py+2)) or ((((round((mx/largura/8))-0.5) == px-2 or round((mx/largura/8))-0.5) == px+2)  and ((round((my/largura/8))-0.5) == py+1))) #Cavalo
-            print("Camelo")
+            loc = ((round((mx/(largura/8))-0.5) == px+1 or round((mx/(largura/8))-0.5) == px-1) and ((round((my/(largura/8))-0.5) == py+3))) or ((round((mx/(largura/8))-0.5) == px+3 or round((mx/(largura/8))-0.5) == px-3) and ((round((my/(largura/8))-0.5) == py+1))) or ((round((mx/(largura/8))-0.5) == px+1 or round((mx/(largura/8))-0.5) == px-1) and ((round((my/(largura/8))-0.5) == py-3))) or ((round((mx/(largura/8))-0.5) == px+3 or round((mx/(largura/8))-0.5) == px-3) and ((round((my/(largura/8))-0.5) == py-1))) 
+            print("Camelo nos Esteroides de Cobre")
         case 2:
-            loc = (round((mx/(largura/8))-0.5) == px+1 and round((my/(largura/8))-0.5) == py-1) or (round((mx/(largura/8))-0.5) == px+2 and round((my/(largura/8))-0.5) == py-2) or (round((mx/(largura/8))-0.5) == px+3 and round((my/(largura/8))-0.5) == py-3)or (round((mx/(largura/8))-0.5) == px+4 and round((my/(largura/8))-0.5) == py-4)or (round((mx/(largura/8))-0.5) == px+5 and round((my/(largura/8))-0.5) == py-5)or (round((mx/(largura/8))-0.5) == px+6 and round((my/(largura/8))-0.5) == py-6)or (round((mx/(largura/8))-0.5) == px+7 and round((my/(largura/8))-0.5) == py-7) or (round((mx/(largura/8))-0.5) == px-1 and round((my/(largura/8))-0.5) == py+1) or (round((mx/(largura/8))-0.5) == px-2 and round((my/(largura/8))-0.5) == py+2) or (round((mx/(largura/8))-0.5) == px-3 and round((my/(largura/8))-0.5) == py+3) or (round((mx/(largura/8))-0.5) == px-4 and round((my/(largura/8))-0.5) == py+4) or (round((mx/(largura/8))-0.5) == px-5 and round((my/(largura/8))-0.5) == py+5) or (round((mx/(largura/8))-0.5) == px-6 and round((my/(largura/8))-0.5) == py+6) or (round((mx/(largura/8))-0.5) == px-7 and round((my/(largura/8))-0.5) == py+7) or (round((mx/(largura/8))-0.5) == px-1 and round((my/(largura/8))-0.5) == py-1) or (round((mx/(largura/8))-0.5) == px-2 and round((my/(largura/8))-0.5) == py-2) or (round((mx/(largura/8))-0.5) == px-3 and round((my/(largura/8))-0.5) == py-3)or (round((mx/(largura/8))-0.5) == px-4 and round((my/(largura/8))-0.5) == py-4)or (round((mx/(largura/8))-0.5) == px-5 and round((my/(largura/8))-0.5) == py-5)or (round((mx/(largura/8))-0.5) == px-6 and round((my/(largura/8))-0.5) == py-6)or (round((mx/(largura/8))-0.5) == px-7 and round((my/(largura/8))-0.5) == py-7) or (round((mx/(largura/8))-0.5) == px+1 and round((my/(largura/8))-0.5) == py+1) or (round((mx/(largura/8))-0.5) == px+2 and round((my/(largura/8))-0.5) == py+2) or (round((mx/(largura/8))-0.5) == px+3 and round((my/(largura/8))-0.5) == py+3) or (round((mx/(largura/8))-0.5) == px+4 and round((my/(largura/8))-0.5) == py+4) or (round((mx/(largura/8))-0.5) == px+5 and round((my/(largura/8))-0.5) == py+5) or (round((mx/(largura/8))-0.5) == px+6 and round((my/(largura/8))-0.5) == py+6) or (round((mx/(largura/8))-0.5) == px+7 and round((my/(largura/8))-0.5) == py+7)
-            print("Sacerdote")
+            loc = round((mx/(largura/8))-0.5) or round((my/(largura/8))-0.5)
+            print("Bonk")
         case 3:
-            loc =  (round((mx/(largura/8))-0.5) == px and (8 >= round((my/(largura/8))-0.5) >= 0)) or (round((my/(largura/8))-0.5) == py and (8 >= round((mx/(largura/8))-0.5) >=0)) #Torre
-            print("Pirâmide")
+            loc = round((mx/(largura/8))-0.5) == px and round((my/(largura/8))-0.5) <= py
+            print("Moon Walk")
         case 4:
-            loc = (round((mx/(largura/8))-0.5) == px+1 and round((my/(largura/8))-0.5) == py-1) or (round((mx/(largura/8))-0.5) == px+2 and round((my/(largura/8))-0.5) == py-2) or (round((mx/(largura/8))-0.5) == px+3 and round((my/(largura/8))-0.5) == py-3)or (round((mx/(largura/8))-0.5) == px+4 and round((my/(largura/8))-0.5) == py-4)or (round((mx/(largura/8))-0.5) == px+5 and round((my/(largura/8))-0.5) == py-5)or (round((mx/(largura/8))-0.5) == px+6 and round((my/(largura/8))-0.5) == py-6)or (round((mx/(largura/8))-0.5) == px+7 and round((my/(largura/8))-0.5) == py-7) or (round((mx/(largura/8))-0.5) == px-1 and round((my/(largura/8))-0.5) == py+1) or (round((mx/(largura/8))-0.5) == px-2 and round((my/(largura/8))-0.5) == py+2) or (round((mx/(largura/8))-0.5) == px-3 and round((my/(largura/8))-0.5) == py+3) or (round((mx/(largura/8))-0.5) == px-4 and round((my/(largura/8))-0.5) == py+4) or (round((mx/(largura/8))-0.5) == px-5 and round((my/(largura/8))-0.5) == py+5) or (round((mx/(largura/8))-0.5) == px-6 and round((my/(largura/8))-0.5) == py+6) or (round((mx/(largura/8))-0.5) == px-7 and round((my/(largura/8))-0.5) == py+7) or (round((mx/(largura/8))-0.5) == px-1 and round((my/(largura/8))-0.5) == py-1) or (round((mx/(largura/8))-0.5) == px-2 and round((my/(largura/8))-0.5) == py-2) or (round((mx/(largura/8))-0.5) == px-3 and round((my/(largura/8))-0.5) == py-3)or (round((mx/(largura/8))-0.5) == px-4 and round((my/(largura/8))-0.5) == py-4)or (round((mx/(largura/8))-0.5) == px-5 and round((my/(largura/8))-0.5) == py-5)or (round((mx/(largura/8))-0.5) == px-6 and round((my/(largura/8))-0.5) == py-6)or (round((mx/(largura/8))-0.5) == px-7 and round((my/(largura/8))-0.5) == py-7) or (round((mx/(largura/8))-0.5) == px+1 and round((my/(largura/8))-0.5) == py+1) or (round((mx/(largura/8))-0.5) == px+2 and round((my/(largura/8))-0.5) == py+2) or (round((mx/(largura/8))-0.5) == px+3 and round((my/(largura/8))-0.5) == py+3) or (round((mx/(largura/8))-0.5) == px+4 and round((my/(largura/8))-0.5) == py+4) or (round((mx/(largura/8))-0.5) == px+5 and round((my/(largura/8))-0.5) == py+5) or (round((mx/(largura/8))-0.5) == px+6 and round((my/(largura/8))-0.5) == py+6) or (round((mx/(largura/8))-0.5) == px+7 and round((my/(largura/8))-0.5) == py+7) or (round((mx/(largura/8))-0.5) == px and (8 >= round((my/(largura/8))-0.5) >= 0)) or (round((my/(largura/8))-0.5) == py and (8 >= round((mx/(largura/8))-0.5) >=0))#Rainha
-            print("Isis")
-        case 5:
-            loc = ((round((mx/(largura/8))-0.5) == px-1 or (round((mx/(largura/8))-0.5) == px +1)) and (round((my/(largura/8))-0.5) == py) or (round((mx/(largura/8))-0.5) == px) or ((round((my/(largura/8))-0.5) == py-1 or round((my/(largura/8))-0.5) == py+1) and  (round((mx/(largura/8))-0.5) == px or round((mx/(largura/8))-0.5) == px)) or (round((mx/(largura/8))-0.5) == px-1 or (round((mx/(largura/8))-0.5) == px+1) and ((round((my/(largura/8))-0.5) == py-1 or round((my/(largura/8))-0.5) == py+1))))#rei
-            print("Hórus")
+            loc = (round((mx/(largura/8))-0.5) == px+1 and round((my/(largura/8))-0.5) <= py+1) or (round((mx/(largura/8))-0.5) == px-1 and round((my/(largura/8))-0.5) <= py-1) or (round((mx/(largura/8))-0.5) == px-1 and round((my/(largura/8))-0.5) <= py+1) or (round((mx/(largura/8))-0.5) == px+1 and round((my/(largura/8))-0.5) <= py-1)
+            print("De Volta para o futuro")
+
     return(loc)
