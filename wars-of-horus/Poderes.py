d083ce49f499eb9a63f590511ff0752aca121f22
KingSoapVII <126209081+KingSoapVII@users.noreply.github.com>
2023-10-21 10:12:57 -0300

    Update Poderes.py
    macelo comendo tudo

--- wars-of-horus/Poderes.py
+++ wars-of-horus/Poderes.py
@@ -8,7 +8,7 @@ def PoderW(mx,largura,my,px,py,poder):
             print("Plot Twist")
 
         case 1:
-            loc = ( (round((mx/(largura/8))-0.5) == px+1 or round((mx/(largura/8))-0.5) == px-1) and ((round((my/(largura/8))-0.5) == py-3)) or ((((round((mx/largura/8))-0.5) == px-3 or round((mx/largura/8))-0.5) == px+3)  and ((round((my/largura/8))-0.5) == py-1)))
+            loc = ((round((mx/(largura/8))-0.5) == px+1 or round((mx/(largura/8))-0.5) == px-1) and ((round((my/(largura/8))-0.5) == py-3))) or ((round((mx/(largura/8))-0.5) == px+3 or round((mx/(largura/8))-0.5) == px-3) and ((round((my/(largura/8))-0.5) == py-1))) or ((round((mx/(largura/8))-0.5) == px+1 or round((mx/(largura/8))-0.5) == px-1) and ((round((my/(largura/8))-0.5) == py+3))) or ((round((mx/(largura/8))-0.5) == px+3 or round((mx/(largura/8))-0.5) == px-3) and ((round((my/(largura/8))-0.5) == py+1)))
             print("Camelo nos Esteroides de Cobre")
 
         case 2:
@@ -25,13 +25,12 @@ def PoderW(mx,largura,my,px,py,poder):
     return(loc)
 
 def PoderB(mx,largura,my,px,py,poder):
-
     match poder:
         case 0:
             loc = round((mx/(largura/8))-0.5) or round((my/(largura/8))-0.5)
             print("Plot Twist")
         case 1:
-            loc = ( (round((mx/(largura/8))-0.5) == px+1 or round((mx/(largura/8))-0.5) == px-1) and ((round((my/(largura/8))-0.5) == py+3)) or ((((round((mx/largura/8))-0.5) == px-3 or round((mx/largura/8))-0.5) == px+3)  and ((round((my/largura/8))-0.5) == py+1)))
+            loc = ((round((mx/(largura/8))-0.5) == px+1 or round((mx/(largura/8))-0.5) == px-1) and ((round((my/(largura/8))-0.5) == py+3))) or ((round((mx/(largura/8))-0.5) == px+3 or round((mx/(largura/8))-0.5) == px-3) and ((round((my/(largura/8))-0.5) == py+1))) or ((round((mx/(largura/8))-0.5) == px+1 or round((mx/(largura/8))-0.5) == px-1) and ((round((my/(largura/8))-0.5) == py-3))) or ((round((mx/(largura/8))-0.5) == px+3 or round((mx/(largura/8))-0.5) == px-3) and ((round((my/(largura/8))-0.5) == py-1))) 
             print("Camelo nos Esteroides de Cobre")
         case 2:
             loc = round((mx/(largura/8))-0.5) or round((my/(largura/8))-0.5)
